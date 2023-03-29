13f37737b373b1ff181aae8b8c7e801c37acbd31
DanyaDankou <93947618+DanyaDankou@users.noreply.github.com>
2023-03-30 01:29:13 +0300

    list roster with photo

--- NHLMyScore/NHLMyScore/CashedAsyncImage/CachedAsyncImageView.swift
+++ NHLMyScore/NHLMyScore/CashedAsyncImage/CachedAsyncImageView.swift
@@ -11,15 +11,21 @@ struct CachedAsyncImageView: View {
     
     let imageURL: URL?
     let imageSize: CGSize
+    let cornerRadius: Double
+    let shadowIsOn: Bool
   
 
     var body: some View {
-        CachedAsyncImage(url: getHeadShotURL(for: Int()), content: { image in
+        
+        CachedAsyncImage(url: imageURL, content: { image in
             image.resizable()
         }, placeholder: {
             ProgressView()
         })
         .frame(width: imageSize.width, height: imageSize.height)
+        .frame(width: imageSize.width, height: imageSize.height)
+        .cornerRadius(cornerRadius)
+        .shadow(radius: shadowIsOn ? 10 : 0)
     }
 }
 
@@ -27,7 +33,9 @@ struct CachedAsyncImageView_Previews: PreviewProvider {
     static var previews: some View {
         CachedAsyncImageView(
             imageURL: URL(string: "https://via.placeholder.com/150/92c952")!,
-            imageSize: CGSize(width: 100, height: 100)
+            imageSize: CGSize(width: 100, height: 100),
+            cornerRadius: 10,
+            shadowIsOn: true
         )
     }
 }
--- NHLMyScore/NHLMyScore/TeamInfoSwiftUI/View/PlayersListView/PlayerRow.swift
+++ NHLMyScore/NHLMyScore/TeamInfoSwiftUI/View/PlayersListView/PlayerRow.swift
@@ -14,19 +14,35 @@ struct PlayerRow: View {
 //    @ObservedObject var viewModelPlayer = PlayerViewModel()
     
     var body: some View {
-        HStack {
-           
+        HStack() {
+
             CachedAsyncImageView(imageURL: getHeadShotURL(for: player.person.id),
-                                 imageSize: CGSize(width: 70, height: 70))
+                                 imageSize: CGSize(width: 70, height: 70),
+                                 cornerRadius: 10,
+                                 shadowIsOn: false)
             
+            VStack(alignment: .leading) {
+                HStack {
             Text(player.person.fullName)
+                    .font(.title)
+                    Spacer()
             Text(player.jerseyNumber)
+                        .font(.title)
+                        .padding(.trailing)
+                }
+            Text(player.position.name)
+                    .font(.subheadline)
+                   
+                }
+            
+            }
     }
 }
-}
 
-//struct PlayerRow_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PlayerRow(player: Player(id: , person: Person(id: 3, fullName: "Patrick Kane", link: "7489234"), jerseyNumber: "88", position: Position(code: "ds", name: "fdsfs", type: "fds", abbreviation: "dsf")))    }
-//}
+
+struct PlayerRow_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerRow(player: Player(id: 8474141, person: Person(id: 3, fullName: "Patrick Kane", link: "7489234"), jerseyNumber: "88", position: Position(code: "ds", name: "fdsfs", type: "fds", abbreviation: "dsf")))
+    }
+}
 
